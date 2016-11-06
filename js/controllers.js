77ebb5416e0d14df147603afe0a69a6cf63a5906
James C. De Ricco <jcd7042@rit.edu>
2016-11-06 13:01:44 -0500

    Replace JSON in controller.js with animals.json.

--- js/controllers.js
+++ js/controllers.js
@@ -12,17 +12,13 @@ environmapController.controller('parkController', ['$scope', function($scope) {
     }
 }]);
 
-environmapController.controller('natureInfoAnimalsController', ['$scope', function($scope) {
-    this.animals = [
-        {
-            "name": "Mule Deer",
-            "images": [ { "url": "content/nature_info/mule_deer_male.jpg", "src_url": "https://en.wikipedia.org/wiki/File:2012-mule-deer-male.jpg" },
-                        { "url": "content/nature_info/mule_deer_female.jpg", "src_url": "https://en.wikipedia.org/wiki/File:2012-mule-deer-female.jpg" } ]
-        },
-        {
-            "name": "Yellow-Bellied Marmot",
-            "images": [ { "url": "content/nature_info/yellow_bellied_marmot.jpg", "src_url": "https://en.wikipedia.org/wiki/File:Marmota_flaviventris_%28Yellow_Bellied_Marmot%29,_Yosemite_NP_-_Diliff.jpg" } ]
-        }
-    ];
+environmapController.controller('natureInfoAnimalsController', ['$scope', '$http', function($scope, $http) {
+    var animalsController = this;
+
+    animalsController.animals = [];
+
+    $http.get('/content/nature_info/animals.json')
+         .then(function(response) { animalsController.animals = response.data; })
+         .catch(function(response) { console.log("Failed to get animals.json."); });
   }
 ]);

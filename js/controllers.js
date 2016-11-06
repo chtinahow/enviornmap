a402d43a689adbd6847dd0f39f88b1a89592d548
James C. De Ricco <jcd7042@rit.edu>
2016-11-06 10:47:20 -0500

    Show animal names and images.
    For now, animals.json is copied into the source of controllers.js.

--- js/controllers.js
+++ js/controllers.js
@@ -11,3 +11,18 @@ environmapController.controller('parkController', ['$scope', function($scope) {
         return $scope.view == viewNum;
     }
 }]);
+
+environmapController.controller('natureInfoAnimalsController', ['$scope', function($scope) {
+    this.animals = [
+        {
+            "name": "Mule Deer",
+            "images": [ { "url": "content/nature_info/mule_deer_male.jpg", "src_url": "https://en.wikipedia.org/wiki/File:2012-mule-deer-male.jpg" },
+                        { "url": "content/nature_info/mule_deer_female.jpg", "src_url": "https://en.wikipedia.org/wiki/File:2012-mule-deer-female.jpg" } ]
+        },
+        {
+            "name": "Yellow-Bellied Marmot",
+            "images": [ { "url": "content/nature_info/yellow_bellied_marmot.jpg", "src_url": "https://en.wikipedia.org/wiki/File:Marmota_flaviventris_%28Yellow_Bellied_Marmot%29,_Yosemite_NP_-_Diliff.jpg" } ]
+        }
+    ];
+  }
+]);

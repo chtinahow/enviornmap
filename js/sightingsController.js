8d94694e64d1a44b15cbc5d11b22c0fc61913008
Justin H <justinhaddock2013@yahoo.com>
2016-11-06 20:49:59 -0500

    Created sightings form

--- js/sightingsController.js
+++ js/sightingsController.js
@@ -2,12 +2,18 @@ var parkSightingsController = angular.module('parkSightingsController', []);
 
 parkSightingsController.controller('sightingsController', ['$scope', function($scope) {
     $scope.view = 0;
+    $(document).ready(function() {
+     $('#filer_input').filer({showThumbs:true});
+});
 
     this.setView = function(newView){
         $scope.view = newView;
     },
 
-    this.isView = function(viewNum){
-        return $scope.view == viewNum;
+    this.isAnimal = function(){
+        if ($scope.view == 1){
+          return true;
+        }
+        return false;
     }
 }]);

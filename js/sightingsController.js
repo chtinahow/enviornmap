43e4be01a8af3e01d9836959b999ba0744c204aa
James C. De Ricco <jcd7042@rit.edu>
2016-11-17 18:38:58 -0500

    Add success message when submitting a sighting.

--- js/sightingsController.js
+++ js/sightingsController.js
@@ -2,12 +2,17 @@ var parkSightingsController = angular.module('parkSightingsController', []);
 
 parkSightingsController.controller('sightingsController', ['$scope', function($scope) {
     $scope.view = 0;
+    this.sightingSubmissionSuccess = false;
 
     this.setView = function(newView){
         $scope.view = newView;
-    },
+    };
 
     this.isView = function(viewNum){
         return $scope.view == viewNum;
-    }
+    };
+
+    $scope.onSightingSubmission = function() {
+      this.sightingSubmissionSuccess = true;
+    };
 }]);

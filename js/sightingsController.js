6118a33b784992cff86257e12b4a88da24042da9
James C. De Ricco <jcd7042@rit.edu>
2016-11-20 00:41:45 -0500

    Use bootstrap for sightings form. Add submit button.
    In order to demonstrate both success and failure, every other time the
    user presses the submit button causes a failure message.

--- js/sightingsController.js
+++ js/sightingsController.js
@@ -2,6 +2,8 @@ var parkSightingsController = angular.module('parkSightingsController', []);
 
 parkSightingsController.controller('sightingsController', ['$scope', function($scope) {
     $scope.view = 0;
+    $scope.alerts = [];
+    var nextAlertID = 0;
 
     this.setView = function(newView){
         $scope.view = newView;
@@ -9,5 +11,25 @@ parkSightingsController.controller('sightingsController', ['$scope', function($s
 
     this.isView = function(viewNum){
         return $scope.view == viewNum;
+    }
+
+    $scope.onSubmitSighting = function() {
+      var msg;
+      var type;
+      if (nextAlertID % 2 == 0) {
+        msg = "Sighting successfully submitted!";
+        type = "success";
+      } else {
+        msg = "Sighting submission failed. Please try again later.";
+        type = "danger";
+      }
+      $scope.alerts = [{id: nextAlertID, type: type, msg: msg}];
+      nextAlertID++;
+    };
+
+    $scope.onCloseAlert = function(alertID) {
+      $scope.alerts = $scope.alerts.filter(function(alert) {
+        return alert.id != alertID;
+      });
     }
 }]);

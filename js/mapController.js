79aa78723b6fe4499e263c0830717d9b512b79a4
James C. De Ricco <jcdericco@localhost.localdomain>
2016-12-01 11:59:08 -0500

    Add an animal marker to the map.

--- js/mapController.js
+++ js/mapController.js
@@ -11,11 +11,29 @@ parkMapController.controller('mapController', ['$scope', function($scope) {
         return $scope.view == viewNum;
     }
 
+    var abstractDefaultIcon = {
+      iconSize: [32,37]
+    };
+
+    var animalIcon = angular.copy(abstractDefaultIcon);
+    animalIcon.iconUrl = "icons/deer.png";
+
+    var animalMarkers = {
+      lat: 44.4270,
+      lng: -110.5880,
+      focus: true,
+      message: "Mule Deer",
+      icon: animalIcon
+    };
+
     angular.extend($scope, {
       mapCenter: {
         lat: 44.4280,
         lng: -110.5885,
         zoom: 10
+      },
+      markers: {
+	  mainMarker: angular.copy(animalMarkers)
       }
     });
 }]);

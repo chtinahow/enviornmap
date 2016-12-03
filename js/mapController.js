f7c20e6f6a9d7df88d2899c4875b9c452f543a09
James C. De Ricco <jcd7042@rit.edu>
2016-12-02 22:13:39 -0500

    Refactor fields in mapController for simplicity.

--- js/mapController.js
+++ js/mapController.js
@@ -5,13 +5,11 @@ parkMapController.controller('mapController', ['$scope', function($scope) {
       iconSize: [32,37]
     };
 
-    var animalIcon = angular.copy(abstractDefaultIcon);
-    animalIcon.iconUrl = "icons/deer.png";
+    var animalIcon = angular.extend({ iconUrl: "icons/deer.png" }, abstractDefaultIcon);
 
     var deerMarker1 = {
       lat: 44.4270,
       lng: -110.5880,
-      focus: true,
       message: "Mule Deer",
       icon: animalIcon
     };
@@ -19,7 +17,6 @@ parkMapController.controller('mapController', ['$scope', function($scope) {
     var deerMarker2 = {
       lat: 44.3560,
       lng: -110.3700,
-      focus: true,
       message: "Mule Deer",
       icon: animalIcon
     };
